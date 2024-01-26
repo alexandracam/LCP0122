b824a9d16d420c2eedc92fb8ee21238b748dccd6
Alexandra Camarena <alexandracamarena@ou.edu>
2024-01-26 10:32:02 -0600

    Add project files.

---
+++ LCP0122/Program.cs
@@ -0,0 +1,19 @@
+﻿// MIS 3013
+// Jan 22 2024
+// Alexandra Camarena
+// 113568154
+
+// define variables
+int age1;
+int age2;// int is the datatype age2 is the variable name
+
+double weight1;
+double weight2;
+
+bool isA;//
+
+char ch;// char is the datatype ch is the variable name
+
+age1 = 20;
+
+Console.WriteLine(age1);
